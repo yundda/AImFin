28c8aa3d085d8d9d20bc07835259065e6d9afc3e
yundda <vvvzzang1218@naver.com>
2025-12-01 16:31:38 +0900

    [Feat]/서버ON

--- backend/portfolios/urls.py
+++ backend/portfolios/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("ping", views.portfolio_ping),  # GET /api/portfolio/ping
-    path("save", views.portfolio_save_test, name="portfolio.save"),  # POST /api/portfolio/save
-    path("list", views.portfolio_list_test, name="portfolio.list"),  # GET /api/portfolio/list
-    path("representative", views.portfolio_representative_test, name="portfolio.representative"),  # GET/POST
-    path("<int:portfolio_id>", views.portfolio_detail_test, name="portfolio.detail"),  # GET /api/portfolio/{id}
+    path("save", views.portfolio_save, name="portfolio.save"),  # POST /api/portfolio/save
+    path("list", views.portfolio_list, name="portfolio.list"),  # GET /api/portfolio/list
+    path("representative", views.portfolio_representative, name="portfolio.representative"),  # GET/POST
+    path("<int:portfolio_id>", views.portfolio_detail, name="portfolio.detail"),  # GET /api/portfolio/{id}
 ]
